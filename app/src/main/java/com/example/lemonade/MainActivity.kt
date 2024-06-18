22ead6cc4037d2114b114361fc314b7d331d4918
Mohammed <mo7amxd.7@gmail.com>
2024-06-18 21:48:26 +0100

    hello

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -5,24 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -31,6 +33,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            LemonadeApp()
 
         }
     }
@@ -38,32 +41,67 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun UiScreen(modifier: Modifier = Modifier) {
-
-
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun PicAndText(modifier: Modifier = Modifier) {
-
-
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally){
-        Image(painter = painterResource(id = R.drawable.lemon_tree)
-            ,
-            contentDescription = null
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = stringResource(id = R.string.lemon_tree1))
+fun LemonadeApp(modifier: Modifier = Modifier) {
+    var currentStep by remember {
+        mutableStateOf(1)
     }
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        when (currentStep) {
+            1 -> {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(text = stringResource(R.string.lemon_tree1))
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Image(
+                        painter = painterResource(R.drawable.lemon_tree),
+                        contentDescription = stringResource(R.string.lemon_tree1),
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .clickable {
+                                currentStep = 2
+                            }
+                    )
+                }
+            }
 
-}
+            2 -> {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(text = stringResource(R.string.squeeze_it))
+                    Spacer(
+                        modifier = Modifier.height(
+                            32
+                                .dp
+                        )
+                    )
+                    Image(
+                        painter = painterResource(R.drawable.lemon_squeeze),
+                        contentDescription = stringResource(R.string.lemon_tree1),
+                        modifier = Modifier.wrapContentSize()
+                    )
+                }
 
-@Preview(showBackground = true)
+            }
+
+        }
+
+
+    }}
+
+@Preview
 @Composable
-private fun hello() {
-    PicAndText(modifier = Modifier.fillMaxSize()
-        .wrapContentSize(Alignment.Center))
-
+private fun ll() {
+    LemonadeApp()
 }
+
+
 
